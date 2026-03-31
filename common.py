f68afa64f06576f930092001c8d441f61af9c3c5
arjun.rajkumar.98@gmail.com <arjun.rajkumar.98@gmail.com>
2026-03-31 17:56:07 +1100

    redo

--- common.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-# DD-MM-YYYY format
-def get_current_date():
-    return datetime.now().strftime("%Y-%m-%d")
-    # Other format - print(date.strftime("%A: %d/%B"))
--- food_database.py
+++
@@ -1,9 +0,0 @@
-# Add sys.argv for -v/--view & -a/--add & -r/--report
-
-# --report: Runs calculation for today's date
-
-# --add: Takes & stores input of regularly eaten foods & macronutrient breakdown. Option to add sugar value
-
-import os
-
-DATA_FILE = os.path.join("data", "food_database.json")
